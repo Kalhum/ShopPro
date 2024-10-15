12f77134278f03aeaad1b3b0663bee134e310cdf
Kalhum <129973108+Kalhum@users.noreply.github.com>
2024-10-15 23:01:22 +0700

    Update login.dart

--- shoppro/lib/login.dart
+++ shoppro/lib/login.dart
@@ -11,18 +11,90 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      leading: IconButton(onPressed: () {}, icon: Icon(Icons.back_hand))),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              print(const Text("back"));
+            },
+            icon: const Icon(Icons.arrow_back)),
+        title: const Text("ShopPro"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                print(const Text("login"));
+              },
+              icon: const Icon(Icons.login))
+        ],
+      ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
         child: Column(
           children: [
-            Icon(Icons.abc),
-            TextField(
-              decoration: InputDecoration(hintText: "Enter username"),
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 20),
             ),
-            TextField(
-              decoration: InputDecoration(hintText: "Enter password"),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                    icon: Icon(Icons.person), hintText: "Enter username"),
+              ),
+            ),
+            Row(
+              children: [
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.lock),
+                      hintText: "Enter password",
+                    ),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      print("Forget?");
+                    },
+                    child: const Text("Forget?"))
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: TextButton(
+                        onPressed: () {
+                          print("Login");
+                        },
+                        child: const Text("Login"),
+                        style: TextButton.styleFrom(
+                            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                            backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            textStyle: const TextStyle(fontSize: 15)),
+                      )),
+                ),
+              ],
+            ),
+            const Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 2,
+                    endIndent: 10,
+                  ),
+                ),
+                Text("OR"),
+                const Expanded(
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 2,
+                    indent: 10,
+                  ),
+                ),
+              ],
             )
           ],
         ),
